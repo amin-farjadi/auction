2f4f2d97245e3079e395c63154822500d0758e82
amin-farjadi <amin.farjadi@outlook.com>
2020-10-19 12:09:39 +0100

    crispy tag added

--- commerce/settings.py
+++ commerce/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'auctions',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +128,6 @@ MEDIA_URL = '/media/'
 
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
